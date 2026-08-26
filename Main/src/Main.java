6d2f35e737fa44af42abcc272059c5dfef9c98c2
Rafael <javdt.rapf@gmail.com>
2026-08-25 23:02:00 -0300

    feat: fraseInvertida + teste.txt

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -19,6 +20,23 @@ public class Main {
             return new ArrayList<>();
         }
     }
+
+    public static List<String> inverterTexto(List<String> lines) {
+        List<String> fraseInvertida = new ArrayList<>();
+        try {
+            Path path = Path.of("C:/teste.txt");
+            lines = Files.readAllLines(path);
+            for (String line : lines) {
+                StringBuilder textoInvertido = new StringBuilder(line).reverse();
+                fraseInvertida.add(textoInvertido.toString());
+            }
+
+        } catch (Exception e) {
+            System.out.println("Erro ao ler o arquivo: ");
+        }
+        return fraseInvertida;
+    }
+
     public static void main(String[] args) {
         List<String> text = readText();
 
